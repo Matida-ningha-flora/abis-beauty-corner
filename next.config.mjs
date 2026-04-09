13847259b091333231599b1c9ad66e048de90829
Matida Flora <matidaflora2gmail.com>
2026-04-09 13:31:20 +0200

    Fix: configuration standalone pour Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: 'standalone',
+  images: {
+    domains: ['images.pexels.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
